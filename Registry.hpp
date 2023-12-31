9b1b1c173aaa9e5859db5feb42809c2db2e37797
Robert Feddeler <RJFeddeler@users.noreply.github.com>
2023-12-31 12:07:56 -0500

    Update Registry.hpp

--- Registry.hpp
+++ Registry.hpp
@@ -1,17 +1,17 @@
 //
 //  Registry.hpp
-//  BasaltEngine
+//  PebbleEngine
 //
 //  Created by Robert Feddeler on 7/30/22.
 //
 
 #pragma once
 
-#include "../BasaltCom.hpp"
+#include "../Core/PebbleCom.hpp"
 #include "StorageSet.hpp"
 
 
-namespace basalt::data {
+namespace pebble {
 
 using Entity = uint64_t;
 using EntityID = uint32_t;
@@ -35,10 +35,10 @@ template <typename T>
 struct uniqueIndex {
   static Component get() {
     static const auto index = indexGenerator::getNextIndex();
-    
+
     return index;
   }
-  
+
   constexpr operator Component() const { return get(); }
 };
 
@@ -58,96 +58,105 @@ static constexpr Entity entityCombine(EntityID id, EntityGeneration gen) {
 
 static constexpr Entity NullEntity = entityCombine(maxValue<EntityID>(), 0);
 
-
 class Registry {
 public:
+  void resetRegistry();
+
   Entity createEntity(bool recycleIfAvailable = true);
   Entity recycleEntity();
   void deleteEntity(Entity entity);
-  
+
   template <typename T>
   ComponentStorageSet<T>* createComponentStorage();
-  
+
   template <typename T>
   ComponentStorageSet<T>* getComponentStorage();
-  
+
   template <typename T>
   T* getComponent(Entity entity);
-  
+
   template <typename T>
   void setComponent(Entity entity, const T& data);
-  
+
+  template <typename T>
+  void setComponent(Entity entity, T&& data);
+
   template <typename T>
   void addComponent(Entity entity);
-  
+
+  template <typename T>
+  void addComponent(Entity entity, const T& data);
+
+  template <typename T>
+  void addComponent(Entity entity, T&& data);
+
   template <typename T>
   void removeComponent(Entity entity);
-  
+
 private:
   template <size_t k, typename T>
   size_t smallestComponent();
-  
+
   template <size_t k, typename T, typename... Ts>
   size_t smallestComponent();
-  
+
   template <typename T>
   size_t smallestComponent();
-  
+
   template <typename T, typename... Ts>
   size_t smallestComponent();
-  
+
   template <typename T>
   void getEntities(Component index, std::vector<Entity>& list);
-  
+
   template <typename T, typename... Ts>
   std::enable_if_t<(sizeof...(Ts) > 0), void>
   getEntities(Component index, std::vector<Entity>& list);
-  
+
   template <typename T, typename... Ts>
   std::vector<Entity> getEntities(Component index);
-  
+
   template <typename T>
   void removeUncommonEntities(std::vector<Entity>& list);
-  
+
   template <typename T, typename... Ts>
   std::enable_if_t<(sizeof...(Ts) > 0), void>
   removeUncommonEntities(std::vector<Entity>& list);
-  
+
 public:
+  template <typename T>
+  uint32_t count();
+
+  template <typename... Ts>
+  std::enable_if_t<(sizeof...(Ts) > 1), uint32_t> count();
+  
   template <typename T, typename Functor>
   void forEach(Functor&& f);
-  
+
   template <typename... Ts, typename Functor>
   std::enable_if_t<(sizeof...(Ts) > 1), void> forEach(Functor&& f);
   
+  template <typename T, typename Functor>
+  void forEachWithEntity(Functor&& f);
   
-  //Delete when done testing
-  void debugCheckContents() {
-    components_.debugCheckContents();
-    
-    std::for_each(components_.begin(),
-                  components_.end(),
-                  [](auto& component){
-      
-      component->debugCheckContents();
-    });
-  }
-  
+  template <typename... Ts, typename Functor>
+  std::enable_if_t<(sizeof...(Ts) > 1), void> forEachWithEntity(Functor&& f);
+
 private:
   template <typename T>
   void componentList(std::vector<Component>& list);
-  
+
   template <typename T, typename... Ts>
-  std::enable_if_t<sizeof...(Ts), void>
+  std::enable_if_t<(sizeof...(Ts) >= 1), void>
   componentList(std::vector<Component>& list);
-  
+
   template <typename T, typename... Ts>
   std::vector<Component> componentList();
-  
+
   std::pair<Component, size_t>
   shortestComponent(const std::vector<Component>& keyList) {
     std::pair<Component, size_t> shortest = { 0, maxValue<size_t>() };
-    
+
     std::for_each(keyList.begin(), keyList.end(), [&](auto key){
       auto ptr = components_.get(key);
       if (ptr) {
@@ -158,14 +167,14 @@ private:
         }
       }
     });
-    
+
     return shortest;
   }
-  
+
 private:
   StorageSet<Component, 0,
       std::unique_ptr<BaseStorageSet<Entity, generationBitCount>>> components_;
-  
+
   std::vector<Entity> entities_;
   EntityID            entityRecyclingHead_ = entityIdentifier(NullEntity);
   size_t              entityRecyclingCount_ = 0;
@@ -193,10 +202,21 @@ T* Registry::getComponent(Entity entity) {
 template <typename T>
 void Registry::setComponent(Entity entity, const T& data) {
   auto ptr = getComponentStorage<T>();
-  
+
   if (!ptr)
     ptr = createComponentStorage<T>();
-  
+
+  if (ptr)
+    ptr->set(entity, data);
+}
+
+template <typename T>
+void Registry::setComponent(Entity entity, T&& data) {
+  auto ptr = getComponentStorage<T>();
+
+  if (!ptr)
+    ptr = createComponentStorage<T>();
+
   if (ptr)
     ptr->set(entity, data);
 }
@@ -204,12 +224,34 @@ void Registry::setComponent(Entity entity, const T& data) {
 template <typename T>
 void Registry::addComponent(Entity entity) {
   auto ptr = getComponentStorage<T>();
-  
+
   if (!ptr)
     ptr = createComponentStorage<T>();
-  
+
   if (ptr)
     ptr->add(entity, {});
+}
+
+template <typename T>
+void Registry::addComponent(Entity entity, const T& data) {
+  auto ptr = getComponentStorage<T>();
+
+  if (!ptr)
+    ptr = createComponentStorage<T>();
+
+  if (ptr)
+    ptr->add(entity, data);
+}
+
+template <typename T>
+void Registry::addComponent(Entity entity, T&& data) {
+  auto ptr = getComponentStorage<T>();
+
+  if (!ptr)
+    ptr = createComponentStorage<T>();
+
+  if (ptr)
+    ptr->add(entity, std::move(data));
 }
 
 template <typename T>
@@ -226,7 +268,7 @@ void Registry::componentList(std::vector<Component>& list) {
 }
 
 template <typename T, typename... Ts>
-std::enable_if_t<sizeof...(Ts), void>
+std::enable_if_t<(sizeof...(Ts) >= 1), void>
 Registry::componentList(std::vector<Component>& list) {
   list.push_back(uniqueIndex<T>());
   componentList<Ts...>(list);
@@ -265,7 +307,7 @@ template <typename T, typename... Ts>
 std::vector<Entity> Registry::getEntities(Component index) {
   std::vector<Entity> list;
   getEntities<T, Ts...>(index, list);
-  
+
   return list;
 }
 
@@ -273,14 +315,14 @@ template <typename T>
 void Registry::removeUncommonEntities(std::vector<Entity>& list) {
   std::vector<Entity> common;
   auto ptr = getComponentStorage<T>();
-  
+
   if (ptr && list.size() > 0) {
     std::for_each(list.begin(), list.end(), [&](Entity& e){
       if (ptr->contains(e))
         common.push_back(e);
     });
   }
-  
+
   list = common;
 }
 
@@ -291,15 +333,86 @@ Registry::removeUncommonEntities(std::vector<Entity>& list) {
   removeUncommonEntities<Ts...>(list);
 }
 
+template <typename T>
+uint32_t Registry::count() {
+  uint32_t counter = 0;
+  auto ptr = getComponentStorage<T>();
+  if (ptr) {
+    size_t idx = 0;
+    std::for_each(ptr->begin(), ptr->end(), [&](auto& value){
+      Entity e = *(ptr->keyBegin() + idx++);
+      if (ptr->contains(e))
+        ++counter;
+    });
+  }
+  
+  return counter;
+}
+
+template <typename... Ts>
+std::enable_if_t<(sizeof...(Ts) > 1), uint32_t> Registry::count() {
+  auto keyList = componentList<Ts...>();
+  auto shortest = shortestComponent(keyList);
+  uint32_t counter = 0;
+
+  if (shortest.second > 0 && shortest.second != maxValue<size_t>()) {
+    auto entityList = getEntities<Ts...>(shortest.first);
+    removeUncommonEntities<Ts...>(entityList);
+
+    std::for_each(entityList.begin(), entityList.end(), [&](Entity& e){
+      ++counter;
+    });
+  }
+  
+  return counter;
+}
+
 template <typename T, typename Functor>
 void Registry::forEach(Functor&& f) {
   auto ptr = getComponentStorage<T>();
-  if (ptr)
-    std::for_each(ptr->begin(), ptr->end(), f);
+  if (ptr) {
+    size_t idx = 0;
+    std::for_each(ptr->begin(), ptr->end(), [&](auto& value){
+      Entity e = *(ptr->keyBegin() + idx++);
+      if (ptr->contains(e))
+        f(value);
+    });
+  }
 }
 
 template <typename... Ts, typename Functor>
 std::enable_if_t<(sizeof...(Ts) > 1), void> Registry::forEach(Functor&& f) {
+  auto keyList = componentList<Ts...>();
+  auto shortest = shortestComponent(keyList);
+
+  if (shortest.second > 0 && shortest.second != maxValue<size_t>()) {
+    auto entityList = getEntities<Ts...>(shortest.first);
+    removeUncommonEntities<Ts...>(entityList);
+
+    std::for_each(entityList.begin(), entityList.end(), [&](Entity& e){
+            auto params = std::tuple<Ts&...>(*getComponent<Ts>(e)...);
+            std::apply(f, params);
+    });
+  }
+}
+
+template <typename T, typename Functor>
+void Registry::forEachWithEntity(Functor&& f)
+{
+  auto ptr = getComponentStorage<T>();
+  if (ptr) {
+    size_t idx = 0;
+    std::for_each(ptr->begin(), ptr->end(), [&](auto& value) {
+      Entity e = *(ptr->keyBegin() + idx++);
+      if (ptr->contains(e))
+        f(e, value);
+    });
+  }
+}
+
+template <typename... Ts, typename Functor>
+std::enable_if_t<(sizeof...(Ts) > 1), void> Registry::forEachWithEntity(Functor&& f)
+{
   auto keyList = componentList<Ts...>();
   auto shortest = shortestComponent(keyList);
   
@@ -307,9 +420,9 @@ std::enable_if_t<(sizeof...(Ts) > 1), void> Registry::forEach(Functor&& f) {
     auto entityList = getEntities<Ts...>(shortest.first);
     removeUncommonEntities<Ts...>(entityList);
     
-    std::for_each(entityList.begin(), entityList.end(), [&](Entity& e){
-            auto params = std::tuple<Ts&...>(*getComponent<Ts>(e)...);
-            std::apply(f, params);
+    std::for_each(entityList.begin(), entityList.end(), [&](Entity& e) {
+      auto params = std::tuple<Entity, Ts&...>(e, *getComponent<Ts>(e)...);
+      std::apply(f, params);
     });
   }
 }
