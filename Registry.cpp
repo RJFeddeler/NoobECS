e2f5f7ff954319ee617c7c3684168c5caefe34a8
Robert Feddeler <RJFeddeler@users.noreply.github.com>
2023-12-31 12:07:38 -0500

    Update Registry.cpp

--- Registry.cpp
+++ Registry.cpp
@@ -1,6 +1,6 @@
 //
 //  Registry.cpp
-//  BasaltEngine
+//  PebbleEngine
 //
 //  Created by Robert Feddeler on 7/30/22.
 //
@@ -8,30 +8,38 @@
 #include "Registry.hpp"
 
 
-namespace basalt::data {
+namespace pebble {
+
+void Registry::resetRegistry()
+{
+  components_.clear();
+  entities_.clear();
+  entityRecyclingHead_ = entityIdentifier(NullEntity);
+  entityRecyclingCount_ = 0;
+}
 
 Entity Registry::createEntity(bool recycleIfAvailable) {
   auto e = recycleIfAvailable ? recycleEntity() : NullEntity;
-  
+
   if (e == NullEntity) {
     auto id = static_cast<EntityID>(entities_.size());
     e = entityCombine(id, 0);
     entities_.push_back(e);
   }
-  
+
   return e;
 }
 
 Entity Registry::recycleEntity() {
   if (entityRecyclingCount_ > 0) {
     auto e = entityCombine(entityRecyclingHead_,
-                           entityGeneration(entities_[entityRecyclingHead_]));
-    
+      entityGeneration(entities_[entityRecyclingHead_]));
+
     entityRecyclingHead_ = entityIdentifier(entities_[entityRecyclingHead_]);
     --entityRecyclingCount_;
-    
+
     entities_[entityIdentifier(e)] = e;
-    
+
     return e;
   }
   else
@@ -42,14 +50,13 @@ void Registry::deleteEntity(Entity entity) {
   auto id = entityIdentifier(entity);
   if (id < entities_.size() && entity == entities_[id]) {
     entities_[id] = entityCombine(entityRecyclingHead_,
-                                  entityGeneration(entities_[id]) + 1);
+      entityGeneration(entities_[id]) + 1);
     entityRecyclingHead_ = id;
     ++entityRecyclingCount_;
-    
-    std::for_each(components_.begin(),
-                  components_.end(),
-                  [entity](auto& component){
-      
+
+    std::for_each(components_.begin(), components_.end(),
+      [entity](auto& component)
+    {
       component->remove(entity);
     });
   }
